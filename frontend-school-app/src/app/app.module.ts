8c642b0afea6ab2befeea21b80b9f8d39a9797ad
Bennet <71608267+bgt0123@users.noreply.github.com>
2023-07-17 18:47:45 +0200

    feat: FE- add sites and header components and first HTML code

--- frontend-school-app/src/app/app.module.ts
+++ frontend-school-app/src/app/app.module.ts
@@ -7,17 +7,38 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from "@angular/router";
+import { TimetableComponent } from './timetable/timetable.component';
+import { RoomComponent } from './room/room.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'student', component: StudentsComponent },
+  { path: 'teacher', component: TeacherComponent },
+  { path: 'timetable', component: TimetableComponent },
+  { path: 'room', component: RoomComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent
+    StudentsComponent,
+    TeacherComponent,
+    TimetableComponent,
+    RoomComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
